3464db1e7d606f1c47314dafd10f082c1ee94edb
Zakkart59 <vinicius.serpafeliciano01@gmail.com>
2022-07-18 03:39:52 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-var nome = "Vinicius Zakkart";
+//Aula de Introdução//
+/*var nome = "Vinicius Zakkart";
 var idade = 19;
 var idade2 = 10;
 var frase = "if(bad_code) return gambiarra";
@@ -9,4 +10,27 @@ console.log(idade + idade2);
 console.log(frase.replace("bad_code", "good_code"));
 console.log(frase.toUpperCase());
 console.log(nome.toLowerCase())
-alert(frase.replace("bad_code", "good_code"));
+alert(frase.replace("bad_code", "good_code"));*/
+
+//Aula Array e Dicionário//
+//array
+/*var lista = ["maçã", "uva", "laranja"];
+//lista.push("pêra");
+//lista.pop();
+console.log(lista );
+alert(lista[1]);
+
+console.log(lista.length); //mostra tamanho da lista
+console.log(lista.reverse); //ordem da lista ao contrário
+console.log(lista.toString()); //transforma array em string
+console.log(lista.join("-")); //troca a separação da string */
+
+//Dicionário
+/*
+var fruta = {nome: "maçã", cor:"vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+var fruta = [{nome: "maçã", cor:"vermelha"}, {nome: "uva", cor:"roxa"}]
+console.log(fruta);
+alert(fruta[1].nome);

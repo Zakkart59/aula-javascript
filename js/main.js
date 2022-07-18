c153a05cad86979843efcf15734fc1826b1f62c1
Zakkart59 <vinicius.serpafeliciano01@gmail.com>
2022-07-18 02:08:04 -0300

    Introdução ao JS

---
+++ js/main.js
@@ -0,0 +1,12 @@
+var nome = "Vinicius Zakkart";
+var idade = 19;
+var idade2 = 10;
+var frase = "if(bad_code) return gambiarra";
+// alert(nome + "tem" + idade + "anos");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("bad_code", "good_code"));
+console.log(frase.toUpperCase());
+console.log(nome.toLowerCase())
+alert(frase.replace("bad_code", "good_code"));
